9903f8d11cdcf38a59ae873d65c72b198b2fa376
Gabriel-Galdino22 <rm550711@fiap.com.br>
2023-08-11 16:46:48 -0300

    Puxando os elementos da index.

---
+++ js/arrays.js
@@ -0,0 +1,7 @@
+// Obtenção de elementos do HTML
+const formularioTarefa = document.getElementById('taskForm');
+const campoDescricaoTarefa = document.getElementById('taskDescription');
+const campoAutorTarefa = document.getElementById('taskAuthor');
+const campoDepartamentoTarefa = document.getElementById('taskDepartment');
+const campoImportanciaTarefa = document.getElementById('taskImportance');
+const listaTarefas = document.getElementById('listaTarefas');
